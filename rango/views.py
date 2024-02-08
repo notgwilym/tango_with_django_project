b38b9d5ebe31fbd45913a796ae700362280db28a
“Gwilym <2792628h@student.gla.ac.uk>
2024-02-08 16:38:58 +0000

    pg 40 exercise tested

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says this is the about page! <br/> <a href='/rango/'>Index</a>")
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
