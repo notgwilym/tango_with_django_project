793a2b5e4fd98f58777d21264cf8a4b0941fa915
“Gwilym <2792628h@student.gla.ac.uk>
2024-02-09 02:09:10 +0000

    visit counter working

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,31 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+def visitor_cookie_handler(request, response):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is cast to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits += 1
+        # Update the last visit cookie now that we have updated the count.
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        # Set the last visit cookie
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    # Update/set the visits cookie
+    response.set_cookie('visits', visits)
+
+
 
 
 def index(request):
@@ -23,8 +48,16 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list_likes
     context_dict['pages'] = pages_list_views
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    # Call function to handle the cookies
+    visitor_cookie_handler(request, response)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Gwilym.'}
